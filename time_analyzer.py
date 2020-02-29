a74a96f413fd899820d501e03e1bcba4f0e92526
Gilmar Lnx <gilmarlnx.ifce@gmail.com>
2020-02-29 15:30:01 -0300

    Implementing function time_analyzer()

--- time_analyzer.py
+++ time_analyzer.py
@@ -7,13 +7,18 @@ def time_analyzer(directory):
 	# terminal.not_implement('time analyzer')
 	if os.path.isdir(directory):
 		filename = "teste.txt"
-		command = "ls -l "+directory+" > "+filename
+		# Command shell to get last date of alteration within paste
+		# Save output of command an variable $(filename)
+		command = "date --reference="+directory+" +%d/%m/%y > "+filename
+		os.system(command)
+		# Read output
 		date = read_output(filename)
 		print(f'{colors.blue}{directory}{colors.reset_color} was changed in: {colors.green}{date}{colors.reset_color}...')
 
 def time_analyzer_without_empty(directory):
 	terminal.not_implement('time analyzer without empty')
 
+# Function responsible for reading output in variable $(filename)
 def read_output(filename):
 	file = open(filename, "r")
 	date = []
